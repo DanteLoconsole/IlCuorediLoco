c11a4c9fb914d7c2dfe36c80c1deed244648d825
DanteLoconsole <dante.loconsole@student.kuleuven.be>
2025-07-11 22:07:21 +0200

    trying to make github page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,12 +20,8 @@ function Home() {
             </section>
 
             <div className="instagram_widget">
-                <script src={"https://cdn.lightwidget.com/widgets/lightwidget.js"} />
-                <iframe
-                    src="https://cdn.lightwidget.com/widgets/8ced8ffe07275bcd92b6256e9f70f430.html"
-                    allowTransparency={false}
-                    className="lightwidget-widget"
-                ></iframe>
+                <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+                <iframe src="https://cdn.lightwidget.com/widgets/8ced8ffe07275bcd92b6256e9f70f430.html" className="lightwidget-widget"/>
             </div>
 
             <section className="newsletter">
